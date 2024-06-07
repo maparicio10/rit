ee9f152967559f2016fb458bdd90e59d1ae5a615
Maikel Aparicio Reytor <maparicio10@gmail.com>
2024-06-07 10:04:10 -0400

    Creating the models.

--- rit/settings.py
+++ rit/settings.py
@@ -126,7 +126,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-us'
+LANGUAGE_CODE = 'es-ar'
 
 TIME_ZONE = 'UTC'
 
